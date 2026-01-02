c2a69ea809d638d389361f7f541950c6bb4e398b
Rahul Gupta <rahuldevit2000@gmail.com>
2026-01-02 23:03:50 +0530

    Update StudentController.java
    Remove Unused Code

--- src/main/java/com/cglia/onetoonemapping/controller/StudentController.java
+++ src/main/java/com/cglia/onetoonemapping/controller/StudentController.java
@@ -1,7 +1,6 @@
 package com.cglia.onetoonemapping.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.cglia.onetoonemapping.entity.Student;
 import com.cglia.onetoonemapping.service.StudentService;
 
@@ -70,3 +68,4 @@ public class StudentController {
 	}
 
 }
+
